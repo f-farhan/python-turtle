e577f0c10874020e18e9981102b479d4bee6e26d
AdityaWadkar <67093170+AdityaWadkar@users.noreply.github.com>
2021-10-01 11:15:54 +0530

    added feature to fill color
    Added feature which can fills red color in created circle. And now user can decide radius of circle

--- circle.py
+++ circle.py
@@ -1,6 +1,12 @@
 # Importing the module
 import turtle
 # initalizing turtle
+
 t = turtle.Turtle()
-radius = 40 #declaring the unit of radius
+
+radius = int(input("Enter radius of circle :") )#declaring the unit of radius
+t.color("red")
+t.begin_fill()
 t.circle(radius) #calling the circle(built-in) method
+t.end_fill()
+time.sleep(5)
